f2f138561fbee66bd725f1247d2aa0b7ca116061
nicoleta.fratila <nicoleta.fratila@softvision.com>
2021-07-09 15:52:04 +0300

    utils-go

---
+++ rest_errors/rest_errors_test.go
@@ -0,0 +1,44 @@
+package rest_errors
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestNewInternalServerError(t *testing.T) {
+	err := NewInternalServerError("this is the message", errors.New("database error"))
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "this is the message", err.Message)
+	assert.EqualValues(t, "internal_server_error", err.Error)
+
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "database error", err.Causes[0])
+}
+
+func TestNewBadRequestError(t *testing.T) {
+	err := NewBadRequestError("this is the message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status)
+	assert.EqualValues(t, "this is the message", err.Message)
+	assert.EqualValues(t, "bad_request", err.Error)
+}
+
+func TestNewNotFoundError(t *testing.T) {
+	err := NewNotFoundError("this is the message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.Status)
+	assert.EqualValues(t, "this is the message", err.Message)
+	assert.EqualValues(t, "not_found", err.Error)
+}
+
+func TestNewUnauthorizedError(t *testing.T) {
+	err := NewUnauthorizedError("this is the message")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusUnauthorized, err.Status)
+	assert.EqualValues(t, "this is the message", err.Message)
+	assert.EqualValues(t, "unauthorized", err.Error)
+}
